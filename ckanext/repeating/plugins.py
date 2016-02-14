21cfa378c89e8d9aca7ad9664d71c0a1aee0089d
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-02-14 20:07:34 +0100

    persist repeating better

--- ckanext/repeating/plugins.py
+++ ckanext/repeating/plugins.py
@@ -5,19 +5,24 @@ from ckan.plugins.toolkit import add_template_directory
 from ckanext.repeating import validators
 
 
-def repeating_get_values(field_name, data):
+def repeating_get_values(field_name, form_blanks, data):
     '''
     Template helper function.
-    Get data from repeating_text field from either field_name (if the
-    field comes from the database) or construct from several field-n -
-    entries in case data wasn't saved yet).
+    Get data from repeating_text-field from either field_name (if the
+    field comes from the database) or construct from several field-N -
+    entries in case data wasn't saved yet, i.e. a validation error occurred.
+    In the first case, show additional <form_blanks> empty fields. In the
+    latter, don't change the form.
     '''
     def mk_value():
         fields = [re.match(field_name + "-\d+", key) for key in data.keys()]
         fields = sorted([r.string for r in fields if r])
-        return [data[f] for f in fields]
+        values = [data[f] for f in fields if data[f]]
+        values = values + [''] * max(form_blanks - len(values), 0)
+        return values
         
-    value = data.get(field_name, mk_value())
+    value = data.get(field_name)
+    value = value + [''] * form_blanks if value else mk_value()
     return value
 
 class RepeatingPlugin(p.SingletonPlugin):
