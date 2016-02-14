ca4661b7b0a453c22537f594c2d5f105b126899e
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-02-14 19:27:27 +0100

    persist repeating fields

--- ckanext/repeating/plugins.py
+++ ckanext/repeating/plugins.py
@@ -1,13 +1,29 @@
+import re
 import ckan.plugins as p
 from ckan.plugins.toolkit import add_template_directory
 
 from ckanext.repeating import validators
 
 
+def repeating_get_values(field_name, data):
+    '''
+    Template helper function.
+    Get data from repeating_text field from either field_name (if the
+    field comes from the database) or construct from several field-n -
+    entries in case data wasn't saved yet).
+    '''
+    def mk_value():
+        fields = [re.match(field_name + "-\d+", key) for key in data.keys()]
+        fields = sorted([r.string for r in fields if r])
+        return [data[f] for f in fields]
+        
+    value = data.get(field_name, mk_value())
+    return value
 
 class RepeatingPlugin(p.SingletonPlugin):
     p.implements(p.IValidators)
     p.implements(p.IConfigurer)
+    p.implements(p.ITemplateHelpers)
 
     def update_config(self, config):
         """
@@ -21,3 +37,7 @@ class RepeatingPlugin(p.SingletonPlugin):
             'repeating_text_output':
                 validators.repeating_text_output,
             }
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {'repeating_get_values': repeating_get_values}
+    
